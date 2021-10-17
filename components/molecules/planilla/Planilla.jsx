1c935d8544ae478a50e13a74af55ba303fc761d6
Uriel Quevedo <uquevedo@atixlabs.com>
2021-10-17 03:59:44 -0300

    chore: planilla styled

--- components/molecules/planilla/Planilla.jsx
+++ components/molecules/planilla/Planilla.jsx
@@ -1,10 +1,34 @@
 import Styles from "./_style.module.scss";
 
-const { formulario, pisador, circulo, hueco, adorno, containerForm } = Styles;
+const {
+  formulario,
+  pisador,
+  circulo,
+  hueco,
+  adorno,
+  containerForm,
+  doblado1,
+  doblado2,
+  dobladoComplicado1,
+  dobladoComplicado2,
+  dobladoComplicado3,
+  dobladoSimple1,
+  dobladoSimple2,
+} = Styles;
 
 const Planilla = ({ children }) => {
   return (
     <div className={formulario}>
+      <div className={doblado2} />
+      <div className={doblado1} />
+
+      <div className={dobladoComplicado1} />
+      <div className={dobladoComplicado2} />
+      <div className={dobladoComplicado3} />
+
+      <div className={dobladoSimple1} />
+      <div className={dobladoSimple2} />
+
       <div className={pisador}>
         <div className={circulo} />
         <div className={hueco} />
