be34759a98efee2a0f0e89fea51e60220f281787
Uriel Quevedo <uquevedo@atixlabs.com>
2021-10-24 20:22:44 -0300

    chore: create partido gif

--- components/molecules/crear-partido-form/CrearPartidoForm.jsx
+++ components/molecules/crear-partido-form/CrearPartidoForm.jsx
@@ -1,8 +1,35 @@
 import PlanillaInput from "../../atoms/planilla-input/PlanillaInput";
 import { crearPartidoInputs } from "../../../utils/forms/crear-partido";
 import Styles from "./_style.module.scss";
+import { useEffect, useState } from "react";
 
 const CrearPartidoForm = () => {
+  const [showGif, setShowGif] = useState(false);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setShowGif(false);
+    }, 2200);
+
+    setTimeout(function () {
+      setShowGif(true);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      {crearPartidoInputs.map((inputProps, key) => (
+        <PlanillaInput key={key} {...inputProps} />
+      ))}
+      {showGif && (
+        <img
+          src="https://c.tenor.com/iLYPmFMHM_0AAAAi/dunk-de-rozan.gif"
+          alt="gif de volcada"
+          style={{ position: "absolute" }}
+        />
+      )}
+    </>
+  );
   return crearPartidoInputs.map((inputProps, key) => (
     <PlanillaInput key={key} {...inputProps} />
   ));
--- components/organisms/planilla-form/PlanillaForm.jsx
+++ components/organisms/planilla-form/PlanillaForm.jsx
@@ -2,24 +2,11 @@ import Styles from "./_style.module.scss";
 import CustomButton from "../../atoms/custom-button/CustomButton";
 import Planilla from "../../molecules/planilla/Planilla";
 import { Form } from "antd";
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 
 const { center } = Styles;
 
 const PlanillaForm = ({ textButton, onFinish, formLayout }) => {
   const [form] = Form.useForm();
-  const [showGif, setShowGif] = useState(false);
-
-  useEffect(() => {
-    setTimeout(function () {
-      setShowGif(false);
-    }, 2200);
-
-    setTimeout(function () {
-      setShowGif(true);
-    }, 1200);
-  }, []);
 
   return (
     <div className={center}>
@@ -29,15 +16,7 @@ const PlanillaForm = ({ textButton, onFinish, formLayout }) => {
         layout="vertical"
         onFinish={onFinish}
       >
-        <Planilla>
-          {formLayout}
-          {showGif && (
-            <motion.img
-              src="https://c.tenor.com/iLYPmFMHM_0AAAAi/dunk-de-rozan.gif"
-              style={{ position: "absolute" }}
-            />
-          )}
-        </Planilla>
+        <Planilla>{formLayout}</Planilla>
         <CustomButton text={textButton} theme="confirm" htmlType="submit" />
       </Form>
     </div>
